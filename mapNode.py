31ec3f963efc5d0aea20b49cc8684d3268997ac8
lunar-mycroft <timothy.l.park+github@gmail.com>
2018-11-18 14:01:06 -0500

    Updated isAdjacent check

--- mapNode.py
+++ mapNode.py
@@ -1,8 +1,9 @@
 class MapNode:
-    def __init__(self,svg): #Constructor.  svg is a string containing the svg info for the map node being created
+    def __init__(self,polygons,colors): #Constructor.  svg is a string containing the svg info for the map node being created
         self.neighbors=[]
         self.color=None
-        self.image=svg
+        self.poly=polygons
+        self.colors=colors
 
 
     def connectTo(self,other):
@@ -16,9 +17,33 @@ class MapNode:
 
         return True
 
+    def getLines(self):
+        res=[]
+        for i, vert in enumerate(self.poly):
+            res.append((self.poly[i-1],vert))
+        return res
+
+    def isAdjacent(self,other,tolerance=0.001):
+        if not isinstance(other,MapNode):
+            return False
+
+        if other in self.neighbors:
+            return True
+
+        #todo: add a check for already confirmed false-connections?
+
+        for line in self.getLines():
+            steepSlope=abs(line[0][0]-line[1][0])>abs(line[0][1]-line[1][1])
+            m1 = (line[0][1]-line[1][1])/(line[0][0]-line[1][0]) if steepSlope else (line[0][0]-line[1][0]) / (line[0][1]-line[1][1])
+            b1 = line[0][1]-m1*line[0][0]
+            for otherLine in other.getLines():
+                #do pretty much the same thing to get 
+
+
     def __str__(self): #This should return the SVG of the mape node, except not colored according to color.
         return "placeholder"
 
-    def possibleColors(self,colorOptions):#returns a list of the possible colors the node could take out of colorOptions
+    def possibleColors(self):#returns a list of the possible colors the node could take out of colorOptions
         return [] #placeholder
 
+    def numConnections(self):
